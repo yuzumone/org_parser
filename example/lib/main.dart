4d600d3c3c3ae867ce326fc66227d0e2c127b487
yuzumone <yuzumone@users.noreply.github.com>
2021-01-01 10:19:03 +0900

    Fix crash issue when long tap text field on iOS
    #22

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: [
         Locale('ja', ''),

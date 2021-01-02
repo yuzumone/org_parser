04331bf807938ec970dfe8a9880a5af49627d28d
yuzumone <yuzumone@outlook.com>
2021-01-02 12:53:11 +0900

    Update home view

--- example/lib/ui/home/home_view.dart
+++ example/lib/ui/home/home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:org_parser_example/data/model/file.dart';
+import 'package:org_parser_example/ui/search/search_view.dart';
+import 'package:org_parser_example/ui/search/search_view_state.dart';
 import 'package:provider/provider.dart';
 import 'package:org_parser_example/ui/agenda/agenda_view.dart';
 import 'package:org_parser_example/ui/file/file_view.dart';
@@ -14,10 +18,27 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var pageIndex =
+        context.select<HomeViewState, int>((state) => state.pageIndex);
+    var files =
+        context.select<HomeViewState, List<File>>((state) => state.files);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Org Mobile'),
         actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => StateNotifierProvider<
+                    SearchViewStateNotifier, SearchViewState>(
+                  create: (_) => SearchViewStateNotifier(),
+                  child: SearchView(files: files),
+                ),
+              ),
+            ),
+          ),
           IconButton(
             icon: Icon(Icons.settings),
             onPressed: () => Navigator.of(context).pushNamed('/preference'),
@@ -25,12 +46,10 @@ class HomeView extends StatelessWidget {
         ],
       ),
       body: Container(
-        child: _listWidgets.elementAt(
-            context.select<HomeViewState, int>((state) => state.pageIndex)),
+        child: _listWidgets.elementAt(pageIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex:
-            context.select<HomeViewState, int>((state) => state.pageIndex),
+        currentIndex: pageIndex,
         onTap: (index) => context.read<HomeViewStateNotifier>().setIndex(index),
         selectedItemColor: Colors.blue,
         items: [

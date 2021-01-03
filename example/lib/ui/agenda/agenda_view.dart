13464b471e35312be02d909679daa64110afdc53
yuzumone <yuzumone@users.noreply.github.com>
2021-01-03 12:07:57 +0900

    Add header
    #24

--- example/lib/ui/agenda/agenda_view.dart
+++ example/lib/ui/agenda/agenda_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:intl/intl.dart';
 import 'package:org_parser_example/data/model/file.dart';
 import 'package:org_parser_example/ui/detail/detail_view.dart';
 import 'package:org_parser_example/ui/detail/detail_view_state.dart';
@@ -8,6 +9,8 @@ import 'package:provider/provider.dart';
 import 'package:org_parser/org_parser.dart';
 
 class AgendaView extends StatelessWidget {
+  final format = DateFormat('EEEE\tdd\tMMM\ty');
+
   @override
   Widget build(BuildContext context) {
     var files =
@@ -17,10 +20,26 @@ class AgendaView extends StatelessWidget {
     var doneKeywords = context
         .select<HomeViewState, List<String>>((state) => state.doneKeywords);
     var agenda = _getAgendaList(files);
+    var now = DateTime.now();
+    var start = now.subtract(Duration(days: now.weekday - 1));
+    var dates = Iterable<int>.generate(7)
+        .map((e) => DateTime(start.year, start.month, start.day + e))
+        .toList();
+    var items = <dynamic>[];
+    items.add(dates.first);
+    dates.skip(1).forEach((x) {
+      items.add(x);
+      agenda.forEach((y) {
+        var diff = y.scheduledDateTime.difference(x);
+        if (0 <= diff.inHours && diff.inHours < 24) {
+          items.add(y);
+        }
+      });
+    });
     return ListView.builder(
-      itemCount: agenda.length,
+      itemCount: items.length,
       itemBuilder: (BuildContext context, int index) =>
-          _buildListTile(context, agenda[index], todoKeywords, doneKeywords),
+          _buildTile(context, items[index], todoKeywords, doneKeywords),
     );
   }
 
@@ -48,6 +67,25 @@ class AgendaView extends StatelessWidget {
     } else {
       return false;
     }
+  }
+
+  Widget _buildTile(BuildContext context, dynamic item,
+      List<String> todoKeyword, List<String> doneKeyword) {
+    if (item is DateTime) {
+      return _buildHeaderTile(context, item);
+    }
+    return _buildListTile(context, item, todoKeyword, doneKeyword);
+  }
+
+  Widget _buildHeaderTile(BuildContext context, DateTime dateTime) {
+    return Container(
+      padding: const EdgeInsets.only(
+          left: 16.0, top: 12.0, right: 16.0, bottom: 12.0),
+      child: Text(
+        format.format(dateTime),
+        style: TextStyle(color: Colors.blue[700]),
+      ),
+    );
   }
 
   Widget _buildListTile(BuildContext context, Headline headline,
